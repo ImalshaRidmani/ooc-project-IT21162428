543c313855624b901b8cb7925df372868ffd3611
ImalshaRidmani <93404465+ImalshaRidmani@users.noreply.github.com>
2022-05-20 21:48:46 +0530

    Create CourseCoordinator.cpp

---
+++ CourseCoordinator.cpp
@@ -0,0 +1,31 @@
+Course_coordinator::Course_coordinator(){
+		strcpy((Cc_Id),"");
+	}
+
+	Course_coordinator::Course_coordinator(char ccid[]){
+		Cc_Id = ccid;
+	}
+	
+	void Course_coordinator::DispalyCourseCoordinatorDetails(char ccid[]){
+		
+	}
+
+	void Course_coordinator::AddCourseCoordinatorDetails(){
+		
+	}
+
+	void Course_coordinator::RemoveCourseCoordinatorDetails(){
+		
+	}
+	
+	void Course_coordinator::reschedules(){
+		
+	}
+
+	void Course_coordinator::setCc_Id(char ccid[]){
+		
+	}
+
+	char Course_coordinator::getCc_Id(){
+		return *Cc_Id;
+	}

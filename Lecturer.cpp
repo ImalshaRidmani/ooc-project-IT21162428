0985217db9ae3e786705b989a26cf95c5306ffd9
IT21162046 <99236417+IT21162046@users.noreply.github.com>
2022-05-20 21:48:53 +0530

    Create Lecturer.cpp
    IT21162046

---
+++ Lecturer.cpp
@@ -0,0 +1,33 @@
+#include"Subject.h"
+
+Lecturer::Lecturer(){
+		strcpy((LecId),"");
+	}
+	
+	Lecturer::Lecturer(char lecid[]){
+		LecId = lecid;
+	}
+	
+	void Lecturer::DispalyLecturerDetails(char lecid[]){
+		
+	}
+	
+	void Lecturer::UploadLecturematerials(){
+		
+	}
+	
+	void Lecturer::AddLecturerDetails(){
+		
+	}
+	
+	void Lecturer::RemoveLecturerDetails(){
+		
+	}
+	
+	void Lecturer::setLecId(char lecid[]){
+		
+	}
+	
+	char Lecturer::getLecId(){
+		return *LecId;
+	}

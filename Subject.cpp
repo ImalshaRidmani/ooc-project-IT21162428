eca1a6fb2c805f7d21056fd59d629f47338e8ce4
ImalshaRidmani <93404465+ImalshaRidmani@users.noreply.github.com>
2022-05-20 21:52:42 +0530

    Create Subject.cpp

---
+++ Subject.cpp
@@ -0,0 +1,37 @@
+	Subject::Subject(){
+		strcpy((SubId),"");
+		strcpy((SubName),"");
+	}
+
+	Subject::Subject(char sunid[], char subname[]){
+		SubId = subid;
+		SubName = subname;
+	}
+	
+	void Subject::DisplaySubjectDetails(){
+		
+	}
+
+ 	void Subject::UpdateSubjectDetails(){
+ 		
+	}
+	
+	void Subject::DeleteSubjectDetails(){
+		
+	}
+	
+	void Subject::setSubId(char subid[]){
+		
+	}
+
+	char Subject::getSubId(){
+		return *SubId;
+	}
+	
+	void Subject::setSubName(char subname[]){
+		
+	}
+	
+	char Subject::getSubName(){
+		return *SubName;
+	}
